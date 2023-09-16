fb3ff7415bea1769c431aeecea8c9ed5bfacce33
Ракчеев Георгий <gosharakcheev@ya.ru>
2023-09-16 13:58:40 +0300

    back fixes

--- okna/catalog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import get_object_or_404, render
-
-from .models import Item
-
-
-def index(request):
-    items = Item.objects.all()
-
-    return render(request, 'catalog/index.html', context={'items': items, })
--- okna/okna/urls.py
+++ okna/okna/urls.py
@@ -1,9 +1,8 @@
-# yatube/urls.py
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
 urlpatterns = [
-    path('', include('catalog.urls', namespace='catalog')),
-    path("admin/", admin.site.urls),
+    path('', views.index),
 
 ]
